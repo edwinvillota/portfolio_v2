1d4cd2ae5d24c0ed3a598f480181428cec97ae7d
edwin <edwin@pop-os.localdomain>
2021-02-06 15:34:36 -0500

    Configure babel and resolve aliases

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,11 @@ module.exports = () => {
         module: {
             rules: [
                 {
-                    test: /\.tsx?$/,
-                    use: 'ts-loader',
+                    test: /\.(ts|js)x?$/,
                     exclude: '/node_modules',
+                    use: {
+                        loader: 'babel-loader'
+                    }
                 }
             ],
         },
