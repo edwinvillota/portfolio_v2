4946f821a8a53e532c56dc44a7bf5b2371377695
edwin <edwin@pop-os.localdomain>
2021-03-22 00:01:48 -0500

    Add themes structure required

--- src/styles/themes/LightTheme.ts
+++ src/styles/themes/LightTheme.ts
@@ -2,12 +2,32 @@ import { DefaultTheme } from 'styled-components';
 
 const LightTheme: DefaultTheme = {
   colors: {
-    main: '#FCA311',
-    secondary: '#14213D',
-    background: '#FFFFFF',
-    gray: '#E5E5E5',
-    white: '#FFFFFF',
-    black: '#000000',
+    main: {
+      primary: '#8739f9',
+      accent: '#37b9f1',
+      grey: '#f2f5f5',
+    },
+    dark: {
+      dark1: '#100f10',
+      dark2: '#1b1b1b',
+      dark3: '#242424',
+    },
+    font: {
+      normal: '#565360',
+      label: '#908e9b',
+      disabled: '#e1dfe9',
+    },
+  },
+  lengths: {
+    sm1: '0.25rem',
+    sm2: '0.5rem',
+    sm3: '0.75rem',
+    md1: '1rem',
+    md2: '1.25rem',
+    md3: '1.5rem',
+    lg1: '2rem',
+    lg2: '3rem',
+    lg3: '4rem',
   },
 };
 
